c7e0d6fead130b702c6f93cb04d4e412f251f00c
kaga <kaga21@gmail.com>
2016-07-20 15:07:27 +1200

    use the demo app to test everything

--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
 TEST_CMD="xcodebuild -scheme AftershipRestKit -workspace ./ios/aftership.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.3' build test"
-TEST_DEMO_APP_CMD="xcodebuild -scheme AfterShipApiDemoiOS -workspace ./ios/aftership.xcworkspace -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.3' clean test"
+TEST_DEMO_APP_CMD="xcodebuild -scheme AfterShipApiDemoiOS -workspace ./ios/aftership.xcworkspace -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.3' build test"
 
 which -s xcpretty
 XCPRETTY_INSTALLED=$?
 
 if [[ $TRAVIS || $XCPRETTY_INSTALLED == 0 ]]; then
-  eval "${TEST_CMD} | xcpretty"
+  #eval "${TEST_CMD} | xcpretty"
   eval "${TEST_DEMO_APP_CMD} | xcpretty"
 else
-  eval "$TEST_CMD"
+  #eval "$TEST_CMD"
   eval "${TEST_DEMO_APP_CMD}"
 fi
